48f232a5d2c2d7818d1d3db936a43b51a29c4520
MmarinoM <marino.michael.1990@gmail.com>
2020-09-10 10:53:49 +0200

    pomodoro

--- src/app.js
+++ src/app.js
@@ -2,13 +2,22 @@
 import React from "react";
 import "bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
+import {Container, Row, Col} from "react-bootstrap";
+import Pomodoro from "./components/pomodoro";
 
 class App extends React.Component {
     render() {
         const {name} = this.props;
         return (
             <>
-                <h1 className={"text-warning"}>{name}</h1>
+                <Container fluid>
+                    <Row className={"h-100"}>
+                        <Col>
+                            <Pomodoro />
+                            {name}
+                        </Col>
+                    </Row>
+                </Container>
             </>
         );
     }
